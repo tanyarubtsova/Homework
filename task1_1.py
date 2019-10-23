00f817ca24f90d1b3c3f5fcbffdf458e196f4c51
tanyarubtsova <55360913+tanyarubtsova@users.noreply.github.com>
2019-10-24 01:33:49 +0300

    Update task1_1.py

--- task1_1.py
+++ task1_1.py
@@ -11,8 +11,11 @@ class MyList:
     def __init__(self):
         self.first = None
         self.last = None
+        self.len = 0
+
 
     def add_node(self, a):
+        self.len += 1
         if self.first == None:
             self.first = Node(a, None)
             self.last = self.first
@@ -20,6 +23,7 @@ class MyList:
             old_node = self.last
             self.last = Node(a, None)
             old_node.next = self.last
+
 
     def find_node(self, a):
         curr = self.first
@@ -34,9 +38,11 @@ class MyList:
         count = 0
         if i == 0:
             self.first = curr.next
+            self.len -= 1
             return
         while curr != None:
             if count == i:
+                self.len -= 1
                 if curr.next == None:
                     self.last = curr
                 prev.next = curr.next
@@ -47,30 +53,32 @@ class MyList:
 
     def print_list(self):
         curr = self.first
+        i = 1
         while curr != None:
-            print(str(curr.key) + ' ', end=' ')
+            if i == self.len:
+                print(str(curr.key), end=' ')
+            else:
+                print(str(curr.key) + ' -> ', end='')
             curr = curr.next
+            i += 1
         print()
+
+def func1(a):
+    l = MyList()
+    while a != '':
+        if (a[0] >= '0') and (a[0] <= '9'):
+            key = a[0]
+            l.add_node(key)
+        a = a[1:]
+    return l
 
 
 def func(a):
     l = MyList()
     while a != '':
-        key = a[-1]
+        if (a[-1] >= '0') and (a[-1] <= '9'):
+            key = a[-1]
+            l.add_node(key)
         a = a[0:-1]
-        l.add_node(key)
     return l
 
-
-a = input()
-l = func(a)
-l.print_list()
-'''
-l = MyList()
-for i in range(3):
-    l.add_node(input())
-l.print_list()
-l.del_node(2)
-l.del_node(0)
-l.print_list()
-'''
