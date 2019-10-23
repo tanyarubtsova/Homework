5c3cdcc2f02a61754187f112cbfaa137ff99b912
tanyarubtsova <55360913+tanyarubtsova@users.noreply.github.com>
2019-10-24 01:31:00 +0300

    Update task2_5.py

--- task2_5.py
+++ task2_5.py
@@ -1,60 +1,65 @@
-'''
-task 2, program 5
-'''
-from collections import deque
+'''task 2 program 5'''
 
 
 class Graph:
     def __init__(self):
-        self.g = []
-        self.n = -1
+        self.graph = []
+        self.count_of_vertexes = -1
 
-    def add_edge(self, b, e):
-        while (self.n < b) or (self.n < e):
-            self.g.append([])
-            self.n += 1
-        self.g[b].append(e)
-        self.g[e].append(b)
-        self.visited = []
+    def add_edge(self, begin, end, weight):
+        while(self.count_of_vertexes < begin) or (self.count_of_vertexes < end):
+            self.graph.append([])
+            self.count_of_vertexes += 1
+        self.graph[begin].append((end, weight))
+        self.graph[end].append((begin, weight))
 
-    def dfs1(self, v):
-        if v not in self.visited:
-            print(v)
-            self.visited.append(v)
-            for i in self.g[v]:
-                self.dfs1(i)
+    def Dijkstra(self, start, end):
+        INF = 1000000000000000
+        dist = [INF] * (self.count_of_vertexes + 1)
+        dist[start] = 0
+        prnt = [0] * (self.count_of_vertexes + 1)
+        used = [False] * (self.count_of_vertexes + 1)
+        for i in range(self.count_of_vertexes + 1):
+            v = -1
+            for j in range (self.count_of_vertexes + 1):
+                if (used[j] == False) and ((v == -1) or (dist[j] < dist[v])):
+                    v = j
+            if (dist[v] == INF):
+                break
+            used[v] = True
+            for j in range(len(self.graph[v])):
+                t = self.graph[v][j][0]
+                l = self.graph[v][j][1]
+                if (dist[v] + l < dist[t]):
+                    dist[t] = dist[v] + l
+                    prnt[t] = v
+        #print(*dist)
+        #print(dist[end])
+        #print(prnt)
+        path = []
+        v = end
+        while v != start:
+            path.append(v)
+            v = prnt[v]
+        path.append(start)
+        path.reverse()
+        print(*path)
 
-    def dfs(self, v):
-        dfs1(v)
-        self.visited = []
-
-    def bfs(self, v):
-        dist = [0] * (self.n + 1)
-        used = [False] * (self.n + 1)
-        q = deque([v])
-        used[v] = True
-        while q:
-            u = q.popleft()
-            #print(u)
-            for v in self.g[u]:
-                if not used[v]:
-                    dist[v] = dist[u] + 1
-                    used[v] = True
-                    q.append(v)
-        return dist
-
-    def dist(self, s, e):
-        print(self.bfs(s)[e])
+    def find_path(self, start, end):
+        self.Dijkstra(start, end)
 
     def print_graph(self):
-        print(self.g)
-'''
+        print(self.graph)
+
 g = Graph()
-for i in range (5):
-    b = int(input())
-    e = int(input())
-    g.add_edge(b, e)
+while 1:
+    s = input().split()
+    if len(s) != 2:
+        b, e, w = [int(i) for i in s]
+        g.add_edge(b, e, w)
+    else:
+        nodeStart, nodeEnd = int(s[0]), int(s[1])
+        break
 #g.print_graph()
-#g.dfs(0)
-g.dist(0, 3)
-'''
+#g.Dijkstra(0)
+g.find_path(nodeStart, nodeEnd)
